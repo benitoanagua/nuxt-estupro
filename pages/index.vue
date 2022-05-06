5f8b5777e3f51ec68e0a659b07fd90da857306e4
Benito Anagua <benito.anagua@gmail.com>
2022-05-06 08:47:34 -0400

    logo has added

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <Logos :logo1="profile.Logo1" :logo2="profile.Logo2" />
     <Hero
       :headline="profile.Headline"
       :illustration="profile.Illustration"
@@ -29,8 +30,4 @@
 const { getItems } = useDirectusItems();
 const profile = await getItems({ collection: "Global" });
 const blocks = await getItems({ collection: "Sections" });
-
-// const containerClass = computed(() => ({
-//   "max-w-screen-md mx-auto px-4 md:px-0": true,
-// }));
 </script>
