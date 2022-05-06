834c8f367bf1bf79d188bb4271013b2ad4e223c4
Benito Anagua <benito.anagua@gmail.com>
2022-05-06 00:29:21 -0400

    youtube player is responsive

--- components/VideoFrame.vue
+++ components/VideoFrame.vue
@@ -1,14 +1,14 @@
 <template>
-  <iframe
-    class="max-w-xl"
-    width="100%"
-    height="315"
-    :src="url"
-    title="YouTube video player"
-    frameborder="0"
-    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen
-  ></iframe>
+  <section class="my-12 flex justify-center">
+    <iframe
+      class="w-full aspect-video max-w-xl"
+      :src="url"
+      title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    ></iframe>
+  </section>
 </template>
 
 <script setup lang="ts">
